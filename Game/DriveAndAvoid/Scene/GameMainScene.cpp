2f53450cce97168b6c5593c00ce895f47b35086a
maedomarimanato <138412497+maedomarimanato@users.noreply.github.com>
2024-02-14 15:21:02 +0900

    更新

--- Game/DriveAndAvoid/Scene/GameMainScene.cpp
+++ Game/DriveAndAvoid/Scene/GameMainScene.cpp
@@ -32,9 +32,9 @@ void GameMainScene::Initialize()
 	int result = LoadDivGraph("Resource/images/3nin.png", 3, 3, 1, 63, 120,
 		enemy_image);
 	  enemy_image[3] = LoadGraph("Resource/images/uparupa.png");
-	int result = LoadDivGraph("Resource/images/car.bmp", 3, 3, 1, 63, 120, enemy_image);
+	result = LoadDivGraph("Resource/images/car.bmp", 3, 3, 1, 63, 120, enemy_image);
 
-	item_image = LoadGraph("Resources/Images/ha-to.png");
+	//item_image = LoadGraph("Resources/Images/ha-to.png");
 	//itemInfos[0].text = "残機回復";
 
 	//itemInfos[2].image = LoadGraph("Resources/Images/無敵.png");
@@ -128,30 +128,30 @@ eSceneType GameMainScene::Update()
 		}
 	}
 
-	//アイテム生成
-	if (mileage / 20 % 100 == 0)
-	{
-		for (int i = 0; i < 10; i++)
-		{
-			if (item == nullptr)
-			{
-				int type = GetRand(3) % 3;
-				item = new Item(type, item_image);
-				item->Initialize();
-				break;
-			}
-		}
-	}
+	////アイテム生成
+	//if (mileage / 20 % 100 == 0)
+	//{
+	//	for (int i = 0; i < 10; i++)
+	//	{
+	//		if (item == nullptr)
+	//		{
+	//			int type = GetRand(3) % 3;
+	//			item = new Item(type, item_image);
+	//			item->Initialize();
+	//			break;
+	//		}
+	//	}
+	//}
 
-	//当たり判定の確認
-	if (IsHitCheck(player, item[i]))
-	{
-		player->SetActive(false);
-		player->DecreaseHp(-50.0f);
-		enemy[i]->Finalize();
-		delete enemy[i];
-		enemy[i] = nullptr;
-	}
+	////当たり判定の確認
+	//if (IsHitCheck(player, item[i]))
+	//{
+	//	player->SetActive(false);
+	//	player->DecreaseHp(-50.0f);
+	//	enemy[i]->Finalize();
+	//	delete enemy[i];
+	//	enemy[i] = nullptr;
+	//}
 	// 障害物生成
 	//if (mileage / 20 % 100 == 0)
 	//{
@@ -183,7 +183,7 @@ void GameMainScene::Draw() const
 	//プレイヤーの描画
 	player->Draw();
 
-	item->Draw();
+	//item->Draw();
 
 	//Ulの描画
 	DrawBox(500, 0, 640, 480, GetColor(0, 153, 0), TRUE);
